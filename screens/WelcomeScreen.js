327a55df360422f9533fc4f973efdb973c1b5364
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-05 21:28:34 -0800

    add mapview

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import Slides from '../components/Slides'; 
-
+import { View, Text, AsyncStorage } from "react-native";
+import Slides from "../components/Slides";
+import { AppLoading } from "expo";
+import _ from "lodash";
 
 const SLIDE_DATA = [
-    {text: 'Welcome To JobApp', color: '#03A9F4' },
-    {text: 'Use this to get a job', color:'#009688' },
-    {text: 'Set your location, then swipe away', color:'#03A9F4' }
-]
+  { text: "Welcome To JobApp", color: "#03A9F4" },
+  { text: "Use this to get a job", color: "#009688" },
+  { text: "Set your location, then swipe away", color: "#03A9F4" }
+];
 
 class WelcomeScreen extends Component {
-  onSlidesComplete = () => {
-      this.props.navigation.navigate('auth');
+  state = { token: null };
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem("fb_token");
+    if (token) {
+      this.props.navigation.navigate("map");
+      this.setState({ token });
+    } else {
+      this.setState({ token: false });
+    }
   }
+
+  onSlidesComplete = () => {
+    this.props.navigation.navigate("auth");
+  };
   render() {
-    return (
-      <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete}/>
-    );
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
+    return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
   }
 }
 
